59b3d7fda34224a4cb3e50cfa8cb5cf8c9b63a1c
NguyenTapLapTrinh <trungnguyenseven7@gmail.com>
2023-02-06 20:38:14 +0700

    change

--- Website/main.js
+++ Website/main.js
@@ -1,4 +1,4 @@
-// Time
+// Time change auto
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
